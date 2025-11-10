ff77f9f6e6bfae0cce0b4a7dc3577d20fa526aa5
vishalverma27-jt <vishalverma29oct@gmail.com>
2025-11-10 12:52:05 +0530

    Binary Search for an array of integers

---
+++ 18.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, arr[100], key;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter sorted elements:\n";
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    int low = 0, high = n - 1;
+
+    while(low <= high) {
+        int mid = (low + high) / 2;
+
+        if(arr[mid] == key) {
+            cout << "Element found at index " << mid;
+            return 0;
+        }
+        else if(key > arr[mid])
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    cout << "Element not found.";
+    return 0;
+}
