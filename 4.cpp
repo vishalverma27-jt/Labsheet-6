d32a8a5fe2662f584b2cf75a5fc6fa7724578117
vishalverma27-jt <vishalverma29oct@gmail.com>
2025-11-10 12:31:24 +0530

    Reverse a 1D array

---
+++ 4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, arr[100];
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter elements:\n";
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    cout << "Reversed array:\n";
+    for(int i = n - 1; i >= 0; i--)
+        cout << arr[i] << " ";
+
+    return 0;
+}
